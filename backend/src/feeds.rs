1a4646c9a87d3968c91f03555e89ca5a4f0d5d77
tyoeer <tyoeer@users.noreply.github.com>
2023-07-20 17:09:03 +0200

    Added feed fetch button

--- backend/src/feeds.rs
+++ backend/src/feeds.rs
@@ -1,4 +1,5 @@
 use leptos::*;
+use leptos_router::ActionForm;
 #[cfg(feature="ssr")]
 use sea_entities::*;
 #[cfg(feature="ssr")]
@@ -36,11 +37,56 @@ pub async fn get_feeds(cx: Scope) -> Result<Vec<FeedInfo>, ServerFnError> {
 	Ok(urls)
 }
 
+#[server(FetchOne, "/api")]
+pub async fn fetch_one_feed(cx: Scope, id: i32) -> Result<i32, ServerFnError> {
+	let conn = use_context::<DatabaseConnection>(cx)
+		.ok_or_else(|| ServerFnError::ServerError("Missing DB connection pool".into()))?;
+	let strats = super::get_strats(cx).await?;
+	
+	let feed = feed::Entity::find_by_id(id).one(&conn).await?;
+	let Some(feed) = feed else {
+		return Err(ServerFnError::ServerError(format!("No feed with id {id}")));
+	};
+	
+	let fetch = strats.run(conn, feed).await;
+	let fetch = match fetch {
+		Ok(f) => f,
+		Err(e) => {
+			tracing::error!("{e:?}");
+			return Err(e.into());
+		}
+	};
+	
+	Ok(fetch.id)
+}
+
 #[component]
 pub fn Feed(cx: Scope, fi: FeedInfo) -> impl IntoView {
+	let fetch_one = create_server_action::<FetchOne>(cx);
+	let button_name = move || {
+		match fetch_one.value().get() {
+			None => {
+				if fetch_one.pending().get() {
+					"fecthing...".to_owned()
+				} else {
+					"fetch".to_owned()
+				}
+			},
+			Some(res) => match res {
+				Ok(id) => format!("fetched: {id}"),
+				Err(err) => format!("server error: {err}"),
+			}
+		}
+	};
 	view! {cx,
 		<span class="table_cell">{fi.name}</span>
 		<span class="table_cell"><a href=&fi.url target="_blank">{fi.url}</a></span>
+		<span class="table_cell">
+			<ActionForm action=fetch_one>
+				<input type="hidden" name="id" value=fi.id/>
+				<input type="submit" value=button_name/>
+			</ActionForm>
+		</span>
 	}
 }
 
