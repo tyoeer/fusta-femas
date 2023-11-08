03c729987558113676a598f448c17455bf07f938
tyoeer <tyoeer@users.noreply.github.com>
2023-11-08 23:22:36 +0100

    Remove client entrypoint from app

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,27 +1,4 @@
-use cfg_if::cfg_if;
 pub mod app;
 pub mod error_template;
 #[cfg(feature = "ssr")]
 pub use backend::layer as extend;
-
-cfg_if! { if #[cfg(feature = "hydrate")] {
-	use wasm_bindgen::prelude::wasm_bindgen;
-	use crate::app::*;
-	use tracing_subscriber::{*, prelude::*};
-	
-	#[wasm_bindgen]
-	pub fn hydrate() {
-		// initializes logging using the `log` crate
-		let fmt_layer = fmt::layer()
-			.with_ansi(false)
-			.with_timer(fmt::time::UtcTime::rfc_3339())
-			.with_writer(tracing_web::MakeConsoleWriter);
-		registry()
-			.with(fmt_layer)
-			.init();
-		
-		console_error_panic_hook::set_once();
-
-		leptos::mount_to_body(App);
-	}
-}}
