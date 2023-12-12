7f3e602d5016714efbe1bc6ed74bcbb703da9914
tyoeer <tyoeer@users.noreply.github.com>
2023-12-12 16:41:20 +0100

    Add some more tests

--- acquire/tests/mock.rs
+++ acquire/tests/mock.rs
@@ -1,9 +1,16 @@
 mod common;
 use common::{init, feed};
 use sea_orm::DbErr;
-use acquire::{strategy, mock::MockStrat};
+use acquire::{
+	strategy::{
+		self,
+		Strategy
+	},
+	mock::MockStrat
+};
 use entities::*;
 
+///A simple test that can copy/pasted to be the basis of other tests
 #[tokio::test]
 async fn basic() -> Result<(), DbErr> {
 	let db = init().await?;
@@ -13,6 +20,56 @@ async fn basic() -> Result<(), DbErr> {
 	let fetch = strategy::run_strategy(db, &feed, &strat).await?;
 	
 	assert_eq!(fetch.status, fetch::Status::Success);
+	
+	Ok(())
+}
+
+///The created fetch looks good
+#[tokio::test]
+async fn fetch() -> Result<(), DbErr> {
+	let db = init().await?;
+	let strat = MockStrat::default();
+	let feed = feed("ok", &strat, &db).await?;
+	
+	let fetch = strategy::run_strategy(db, &feed, &strat).await?;
+	
+	assert_eq!(fetch.status, fetch::Status::Success);
+	assert_eq!(fetch.feed_id, feed.id);
+	assert_eq!(fetch.strategy, strat.name());
+	assert!(fetch.error.is_none());
+	assert!(fetch.content.is_some());
+	
+	Ok(())
+}
+
+///Thinks look alright when a fetch error occurred
+#[tokio::test]
+async fn fetch_error() -> Result<(), DbErr> {
+	let db = init().await?;
+	let strat = MockStrat::default();
+	let feed = feed("fetch error", &strat, &db).await?;
+	
+	let fetch = strategy::run_strategy(db, &feed, &strat).await?;
+	
+	assert_eq!(fetch.status, fetch::Status::FetchError);
+	assert!(fetch.error.is_some());
+	assert!(fetch.content.is_none());
+	
+	Ok(())
+}
+
+///Thinks look alright when a parse error occurred
+#[tokio::test]
+async fn parse_error() -> Result<(), DbErr> {
+	let db = init().await?;
+	let strat = MockStrat::default();
+	let feed = feed("parse error", &strat, &db).await?;
+	
+	let fetch = strategy::run_strategy(db, &feed, &strat).await?;
+	
+	assert_eq!(fetch.status, fetch::Status::ParseError);
+	assert!(fetch.error.is_some());
+	assert!(fetch.content.is_some());
 	
 	Ok(())
 }
