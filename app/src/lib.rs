86d3fadf9499841df4d49168a3e039e3d79b104d
tyoeer <tyoeer@users.noreply.github.com>
2024-02-02 18:06:00 +0100

    Remove no longer used setup stuff in app crate

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -8,21 +8,3 @@ pub mod strategies;
 
 pub mod table;
 pub mod utils;
-
-
-#[cfg(feature="ssr")]
-pub fn extend(router: axum::routing::Router) -> axum::routing::Router {
-	use acquire::*;
-	use axum::Extension;
-	
-	router
-		.layer(Extension( {
-			let mut list = StrategyList::new();
-			list.add(mock::MockStrat::default());
-			list.add(yt_dlp::YtDlpStrategy::default());
-			list
-		} ))
-		.layer(Extension(
-			batch_tracker::BatchTracker::default()
-		))
-}
