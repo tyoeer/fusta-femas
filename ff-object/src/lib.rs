9d2afc930e781bd9e58f3fb055a5d108cfcc9034
tyoeer <tyoeer@users.noreply.github.com>
2024-02-07 12:31:15 +0100

    Move ff_object traits to own submodule

--- ff-object/src/lib.rs
+++ ff-object/src/lib.rs
@@ -4,80 +4,24 @@ pub mod fields;
 mod object_ref;
 pub use object_ref::ObjRef;
 
+///Traits for representing object behaviour
+pub mod traits;
+pub use traits::Object;
+#[cfg(feature="orm")]
+pub use traits::View;
+
+
 
 use leptos::{
 	SignalWith,
 	Signal,
 	IntoSignal,
 };
-
-///Represents an object which is stored in a row in the database
-pub trait Object {
-	fn get_id(&self) -> i32;
-	fn get_object_name() -> &'static str where Self: Sized;
-	
-	fn get_ref(&self) -> ObjRef<Self> where Self: Sized {
-		self.get_id().into()
-	}
-}
-
 use Object as ObjectTrait;
+
+///Returns a [Signal] containing an ObjRef for the given signal containing an object
 pub fn ref_signal<Object: ObjectTrait>(object: impl SignalWith<Value=Object> + 'static) -> Signal<ObjRef<Object>> {
 	let closure = move || object.with(|object| object.get_ref());
 	closure.into_signal()
 }
 
-#[cfg(feature="orm")]
-use sea_orm::{
-	Select,
-	SelectModel,
-	Selector,
-	QuerySelect,
-	FromQueryResult,
-	EntityTrait,
-	ColumnTrait,
-};
-
-/**
-
-A struct that can be queried from the database.
-Provides conveniences that combine [FromQueryResult] and an [Entity](EntityTrait).
-
-The methods you want to edit are:
-- [`columns`](View::columns)
-- [`order`](View::order)
-
-*/
-#[cfg(feature="orm")]
-pub trait View: FromQueryResult {
-	///Represents the table from which this struct gets its data
-	type Entity: EntityTrait;
-	
-	///Specifies which columns this struct selects
-	fn columns() -> impl Iterator<Item = impl ColumnTrait>;
-	
-	///Modifies `query` to be sorted according to a standard
-	fn order(query: Select<Self::Entity>) -> Select<Self::Entity> {
-		query
-	}
-	
-	
-	fn query(modifier: impl FnOnce(Select<Self::Entity>) -> Select<Self::Entity>) -> Selector<SelectModel<Self>> {
-		let query = Self::Entity::find();
-		let query = modifier(query);
-		Self::from_query(query)
-	}
-	
-	
-	fn from_query(query: Select<Self::Entity>) -> sea_orm::Selector<SelectModel<Self>> {
-		let query = Self::order(query);
-		let query = Self::select_only_columns(query);
-		query.into_model::<Self>()
-	}
-	
-	fn select_only_columns(query: Select<Self::Entity>) -> Select<Self::Entity> {
-		query
-			.select_only()
-			.columns(Self::columns())
-	}
-}
---
+++ ff-object/src/traits.rs
@@ -0,0 +1,68 @@
+use crate::ObjRef;
+
+///Represents an object which is stored in a row in the database
+pub trait Object {
+	fn get_id(&self) -> i32;
+	fn get_object_name() -> &'static str where Self: Sized;
+	
+	fn get_ref(&self) -> ObjRef<Self> where Self: Sized {
+		self.get_id().into()
+	}
+}
+
+
+
+#[cfg(feature="orm")]
+use sea_orm::{
+	Select,
+	SelectModel,
+	Selector,
+	QuerySelect,
+	FromQueryResult,
+	EntityTrait,
+	ColumnTrait,
+};
+
+/**
+
+A struct that can be queried from the database.
+Provides conveniences that combine [FromQueryResult] and an [Entity](EntityTrait).
+
+The methods you want to edit are:
+- [`columns`](View::columns)
+- [`order`](View::order)
+
+*/
+#[cfg(feature="orm")]
+pub trait View: FromQueryResult {
+	///Represents the table from which this struct gets its data
+	type Entity: EntityTrait;
+	
+	///Specifies which columns this struct selects
+	fn columns() -> impl Iterator<Item = impl ColumnTrait>;
+	
+	///Modifies `query` to be sorted according to a standard
+	fn order(query: Select<Self::Entity>) -> Select<Self::Entity> {
+		query
+	}
+	
+	
+	fn query(modifier: impl FnOnce(Select<Self::Entity>) -> Select<Self::Entity>) -> Selector<SelectModel<Self>> {
+		let query = Self::Entity::find();
+		let query = modifier(query);
+		Self::from_query(query)
+	}
+	
+	
+	fn from_query(query: Select<Self::Entity>) -> sea_orm::Selector<SelectModel<Self>> {
+		let query = Self::order(query);
+		let query = Self::select_only_columns(query);
+		query.into_model::<Self>()
+	}
+	
+	fn select_only_columns(query: Select<Self::Entity>) -> Select<Self::Entity> {
+		query
+			.select_only()
+			.columns(Self::columns())
+	}
+}
