b09b928485e775b476f5539dbe19a34af469e814
tyoeer <tyoeer@users.noreply.github.com>
2024-02-25 17:14:03 +0100

    ff-macros: fix some docs

--- ff-macros/src/lib.rs
+++ ff-macros/src/lib.rs
@@ -10,7 +10,7 @@ mod field_list;
 
 
 /**
-Generates an impl for [ff_objects::Object].
+Generates an impl for [ff_object::Object].
 
 Id field selection:
 1. first field named "id"
@@ -30,7 +30,7 @@ pub fn object_derive(input: TokenStream) -> TokenStream {
 
 
 /**
-Generates an impl for [ff_objects::fields::FieldListable].
+Generates an impl for [`ff_object::fields::FieldListable`](ff_object::fields::FieldListable)`<`[`dyn bevy_reflect::Reflect`](dyn bevy_reflect::Reflect)`>`.
 */
 #[proc_macro_error]
 #[proc_macro_derive(FieldList)]
