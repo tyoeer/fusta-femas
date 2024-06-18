ac079c46c0bbc4a0bb8839934897e466564f3cc0
tyoeer <tyoeer@users.noreply.github.com>
2024-06-18 23:17:38 +0200

    ffilter: make crate usable on the client

--- ffilter/src/lib.rs
+++ ffilter/src/lib.rs
@@ -9,8 +9,11 @@ For a high-level overview of the filter life-cycle, see [app::query](../app/quer
 
 pub mod shared;
 
+cfg_if::cfg_if! { if #[cfg(feature = "server")] {
 
 pub mod filter;
 pub mod filter_list;
 ///Actual filters
 pub mod filters;
+
+}}
